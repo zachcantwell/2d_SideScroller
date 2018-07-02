d37df28b31fc6872c797b01f174d63a206cd576a
bobbynfld <bobbynfld@gmail.com>
2018-07-02 16:12:27 -0700

    I made two *Sorta* working PlayerController Scripts
    see title

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,254 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput; 
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float _runMultiplier = 1f; 
+	[SerializeField]
+	private float _minJumpVelocity = 5f; 
+	[SerializeField]
+	private float  _climbMultiplier = 2f;
+	[SerializeField]
+	private float _holdJumpButtonMultiplier = 0.5f; 
+	[SerializeField]
+	private float _holdJumpButtonOffset = 1.75f;
+	[SerializeField]
+	private float _gravityScaleOffset = 1.75f; 
+
+	private enum PlayerState{Idle, Running, Jumping, Climbing};
+	private PlayerState _playerState;  
+
+	private float _holdJumpButtonDefaultValue;
+	private float _peakJumpHeight;
+	private float _defaultGravityScale;  
+
+	bool _isAlive; 
+	bool _isPlayerRunning;
+	bool _isPlayerJumping; 
+	bool _isPlayerHoldingJumpButton;
+	bool _isPlayerClimbing;
+	bool _isPlayerTouchingLadder; 
+	bool _wasMaxHeightJumped; 
+
+	Rigidbody2D _rigidbody; 
+	Animator _animator; 
+	CapsuleCollider2D _collider2D; 
+
+	void Start () {
+		_rigidbody = GetComponent<Rigidbody2D>(); 
+		_animator = GetComponent<Animator>(); 
+		_collider2D = GetComponent<CapsuleCollider2D>();
+		_holdJumpButtonDefaultValue = _holdJumpButtonMultiplier; 
+		_defaultGravityScale = _rigidbody.gravityScale; 
+		_isAlive = true; 
+		_isPlayerHoldingJumpButton = false; 
+		_isPlayerClimbing = false; 
+		_isPlayerRunning = false; 
+		_isPlayerJumping = false; 
+		_isPlayerTouchingLadder = false; 
+		_playerState = PlayerState.Idle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_isPlayerRunning = IsPlayerRunning();
+		_isPlayerTouchingLadder = IsPlayerTouchingLadder(); 
+		_isPlayerClimbing = IsPlayerClimbingLadder(_isPlayerTouchingLadder);
+		_isPlayerJumping = IsPlayerJumping(); 
+		_isPlayerHoldingJumpButton =  IsJumpBeingHeld();
+
+
+		if(!_isPlayerHoldingJumpButton)
+		{
+			// _peakJumpHeight = Mathf.NegativeInfinity;
+			// _holdJumpButtonMultiplier = _holdJumpButtonDefaultValue; 
+			// _rigidbody.gravityScale = _defaultGravityScale;
+		}
+
+ 	}
+
+
+	void FixedUpdate()
+	{
+		if(_isPlayerRunning)
+		{
+			FlipSprite(Run());
+			_isPlayerRunning = false; 
+		}
+
+		if(_isPlayerClimbing)
+		{
+			_rigidbody.gravityScale = 0f; 
+			Climbing();
+		}
+
+		if(_isPlayerJumping)
+		{
+			Jump(); 
+			_isPlayerJumping = false; 
+		}
+
+		if(_isPlayerHoldingJumpButton) //  && !_isPlayerClimbing)
+		{
+			if( transform.position.y > _peakJumpHeight)
+			{
+				_holdJumpButtonMultiplier += Time.fixedDeltaTime * _holdJumpButtonOffset;
+				_rigidbody.gravityScale -= Time.fixedDeltaTime * _gravityScaleOffset;
+				_peakJumpHeight = transform.position.y; 
+				_rigidbody.AddForce(new Vector2(0f, _rigidbody.velocity.y) * _holdJumpButtonMultiplier, ForceMode2D.Force);
+			}
+			else
+			{
+				_holdJumpButtonMultiplier = _holdJumpButtonDefaultValue;
+				_rigidbody.gravityScale = _defaultGravityScale;
+			}
+			_isPlayerHoldingJumpButton = false; 
+		}
+		else 
+		{
+			_peakJumpHeight = Mathf.NegativeInfinity;
+			_holdJumpButtonMultiplier = _holdJumpButtonDefaultValue; 
+
+			//TODO This will always be shitty
+			if(_isPlayerClimbing || _isPlayerTouchingLadder)
+			{
+				_rigidbody.gravityScale = 0f;
+			}
+			else
+			{
+				_rigidbody.gravityScale = _defaultGravityScale;
+			}
+		}
+		_isPlayerClimbing = false;
+
+	}
+
+	private bool IsPlayerRunning()
+	{
+		float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); 
+		float playerX = controlThrow * _runMultiplier  * Time.deltaTime; 
+
+        Vector2 playerVelocity = new Vector2(playerX, _rigidbody.velocity.y );
+
+		bool isPlayerMoving = Mathf.Abs(playerVelocity.x) > Mathf.Epsilon; 
+		return isPlayerMoving; 
+	}
+	
+
+    private bool Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); 
+		float playerX = controlThrow * _runMultiplier  * Time.fixedDeltaTime; 
+
+        Vector2 playerVelocity = new Vector2(playerX, _rigidbody.velocity.y );
+		_rigidbody.velocity = playerVelocity;  
+
+		bool isPlayerMoving = Mathf.Abs(_rigidbody.velocity.x) > Mathf.Epsilon; 
+		_animator.SetBool("isRunning", isPlayerMoving); 
+
+		return isPlayerMoving; 
+    }
+
+	private void FlipSprite(bool isHeMoving)
+	{
+		if(isHeMoving)
+		{
+			transform.localScale = new Vector2(Mathf.Sign(_rigidbody.velocity.x), transform.localScale.y);
+		}
+	}
+
+	private bool IsPlayerJumping()
+	{
+		if(!_collider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+		{
+			return false;
+		}
+		if(CrossPlatformInputManager.GetButtonDown("Jump"))
+		{
+			return true; 
+		}
+		return false; 
+	}
+
+	private void Jump()
+	{
+		Vector2 jumpVelocityToAdd = new Vector2(0f, _minJumpVelocity * Time.fixedDeltaTime); 
+		_rigidbody.AddForce(Vector2.up * jumpVelocityToAdd, ForceMode2D.Impulse);
+	}
+
+	private bool IsJumpBeingHeld()
+	{
+		if(CrossPlatformInputManager.GetButton("Jump"))
+		{
+			return true; 
+		}
+		else
+		{
+			_holdJumpButtonMultiplier = 0f; 
+			return false; 
+		}
+	}
+
+	private bool IsPlayerTouchingLadder()
+	{
+		bool isPlayerTouchingLadder = _collider2D.IsTouchingLayers(LayerMask.GetMask("Ladder"));
+
+		if(!isPlayerTouchingLadder)
+		{
+			_animator.SetBool("isClimbing", false);
+			return false;
+		}
+		else
+		{
+			return true; 
+		}
+	}
+
+	private bool IsPlayerClimbingLadder(bool isTouchingLadder)
+	{
+		if(isTouchingLadder)
+		{
+			float controlThrow = CrossPlatformInputManager.GetAxis("Vertical"); 
+			float playerY = controlThrow * _climbMultiplier  * Time.deltaTime; 
+			bool isPlayerClimbing = Mathf.Abs(playerY) > Mathf.Epsilon; 
+			bool isGrounded = _collider2D.IsTouchingLayers(LayerMask.GetMask("Ground"));
+
+			if(isPlayerClimbing || (isTouchingLadder && !isGrounded))
+			{
+				return true;
+			}
+		}
+		return false; 
+	}
+
+	private void Climbing()
+	{
+		float controlThrowY = CrossPlatformInputManager.GetAxis("Vertical"); 
+		float climbSpdY = controlThrowY * _climbMultiplier  * Time.fixedDeltaTime; 
+
+		float controlThrowX = CrossPlatformInputManager.GetAxis("Horizontal"); 
+		float climbSpdX = controlThrowX * _climbMultiplier  * Time.fixedDeltaTime; 
+
+        Vector2 playerVelocity = new Vector2(climbSpdX, climbSpdY);
+		_rigidbody.velocity = playerVelocity;
+		bool isPlayerClimbingY = Mathf.Abs(_rigidbody.velocity.y) > Mathf.Epsilon; 
+		bool isPlayerClimbingX = Mathf.Abs(_rigidbody.velocity.x) > Mathf.Epsilon; 
+
+		if(isPlayerClimbingY || isPlayerClimbingX)
+		{
+			_animator.SetBool("isClimbing", true);
+			_animator.speed = 1f; 
+
+		} 
+		else
+		{
+			//_animator.SetBool("isClimbing", true);
+			_animator.speed = 0f; 
+		}
+	}
+	
+
+}
---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,235 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _maxSpeed = 6f;
+    [SerializeField]
+    private float _jumpPower = 20f;
+    [SerializeField]
+    private float _jumpMultiplier = 20f;
+    [SerializeField]
+    private float _jumpOffset = 1f;
+	[SerializeField]
+	private float _doubleJumpStrength = 0.8f;
+    private float _defaultJumpMultiplier;
+    private float _defaultJumpOffset;
+    private float _currentSpeed = 0f;
+
+    private bool _isJumping;
+    private bool _isHoldingJumpButton;
+    private bool _isDoubleJumpReady;
+
+    private Rigidbody2D _rb;
+    private SpriteRenderer _spriteRenderer;
+    private CapsuleCollider2D _collider2D;
+    private int _doubleJumpCounter;
+
+    public float jumpShortSpeed = 3f;   // Velocity for the lowest jump
+    public float jumpSpeed = 6f;          // Velocity for the highest jump
+    bool jump = false;
+    bool jumpCancel = false;
+    bool doubleJump = false;
+    bool doubleJumpCancel = false;
+    bool grounded = false;
+
+
+    void Awake()
+    {
+        _collider2D = GetComponent<CapsuleCollider2D>();
+        _defaultJumpMultiplier = _jumpMultiplier;
+        _defaultJumpOffset = _jumpOffset;
+        _rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Start()
+    {       
+	    _isHoldingJumpButton = false;
+        _isDoubleJumpReady = false;
+        _isJumping = false;
+        _doubleJumpCounter = 0;
+    }
+
+    void Update()
+    {
+        _currentSpeed = Input.GetAxis("Horizontal") * _maxSpeed;
+
+        grounded = _collider2D.IsTouchingLayers(LayerMask.GetMask("Ground"));  // Check if player is grounded however you like
+
+        if (_doubleJumpCounter < 2)
+        {
+            if (Input.GetButtonUp("Jump") && !grounded)
+            {
+                _doubleJumpCounter++;
+                jumpCancel = true;
+            }
+            if (Input.GetButtonDown("Jump") && _isDoubleJumpReady)
+            {
+                doubleJump = true;
+            }
+        }
+		else if(grounded)
+		{
+			_doubleJumpCounter = 0;
+		}
+
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        _rb.velocity = new Vector2(_currentSpeed, _rb.velocity.y);
+        ChangeDirection();
+    }
+
+    void ChangeDirection()
+    {
+        if (_currentSpeed > 0f && _spriteRenderer.flipX)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (_currentSpeed < 0 && !_spriteRenderer.flipX)
+        {
+            _spriteRenderer.flipX = true;
+        }
+    }
+
+    void Jump()
+    {
+        if (!_isDoubleJumpReady)
+        {
+            _isDoubleJumpReady = true;
+        }
+
+        if (_isDoubleJumpReady)
+        {
+            if (jumpCancel)
+            {
+                if (_rb.velocity.y > jumpShortSpeed)
+                {
+					if(_doubleJumpCounter < 1)
+					{
+						_rb.velocity = new Vector2(_rb.velocity.x, jumpShortSpeed);
+					}
+					else
+					{
+						_rb.velocity = new Vector2(_rb.velocity.x, jumpShortSpeed * _doubleJumpStrength);
+					}
+                }
+                jumpCancel = false;
+            }
+            if (doubleJump)
+            {
+				if(_doubleJumpCounter < 1)
+				{
+					_rb.velocity = new Vector2(_rb.velocity.x, jumpSpeed);
+				}
+				else
+				{
+					_rb.velocity = new Vector2(_rb.velocity.x, jumpSpeed * _doubleJumpStrength);
+				}
+                doubleJump = false;
+            }
+        }
+
+    }
+}
+
+
+//**************************************************************************** */
+//*******************THE BELOW CODE WAS THE CRAPPY WAY I GOT DOUBLE JUMP WORKING */
+    // void Update()
+    // {
+    //     _currentSpeed = Input.GetAxis("Horizontal") * _maxSpeed;
+
+    //     grounded = _collider2D.IsTouchingLayers(LayerMask.GetMask("Ground"));  // Check if player is grounded however you like
+
+    //     if (_doubleJumpCounter < 2)
+    //     {
+    //         // if (Input.GetButtonDown("Jump") && grounded)
+    //         // {
+    //         //     jump = true;
+    //         // }
+    //         if (Input.GetButtonUp("Jump") && !grounded)
+    //         {
+    //             _doubleJumpCounter++;
+    //             Debug.Log(_doubleJumpCounter + " = JumpCtr");
+    //             jumpCancel = true;
+    //         }
+
+    //         if (Input.GetButtonDown("Jump") && _isDoubleJumpReady)
+    //         {
+    //             doubleJump = true;
+    //         }
+    //         // if (Input.GetButtonUp("Jump") && !grounded && !_isDoubleJumpReady)
+    //         // {
+    //         //     doubleJumpCancel = true;
+    //         // }
+    //     }
+	// 	else if(grounded)
+	// 	{
+	// 		_doubleJumpCounter = 0;
+	// 	}
+
+    // }
+
+	//     void Jump()
+    // {
+    //     if (!_isDoubleJumpReady)
+    //     {
+    //         // if (jump)
+    //         // {
+    //         //     _rb.velocity = new Vector2(_rb.velocity.x, jumpSpeed);
+    //         //     Debug.Log("Hit 'Jump'");
+	// 		// 	jump = false;
+    //         // }
+    //         // Cancel the jump when the button is no longer pressed
+    //         _isDoubleJumpReady = true;
+    //     }
+
+    //     if (_isDoubleJumpReady)
+    //     {
+    //         if (jumpCancel)
+    //         {
+    //             if (_rb.velocity.y > jumpShortSpeed)
+    //             {
+    //                 _rb.velocity = new Vector2(_rb.velocity.x, jumpShortSpeed);
+	// 				Debug.Log("Hit 'JumpCancel'");
+    //             }
+    //             jumpCancel = false;
+    //         }
+    //         if (doubleJump)
+    //         {
+    //             Debug.Log("Hit 'DoubleJump'");
+    //             _rb.velocity = new Vector2(_rb.velocity.x, jumpSpeed);
+    //             doubleJump = false;
+    //         }
+    //         // Cancel the jump when the button is no longer pressed
+    //        // if (doubleJumpCancel)
+    //         //{
+    //           //  Debug.Log("Hit 'DoubleJumpCANCEL'");
+
+    //             //if (_rb.velocity.y > jumpShortSpeed)
+    //            // {
+    //                // _rb.velocity = new Vector2(_rb.velocity.x, jumpShortSpeed);
+    //             //}
+    //             //doubleJumpCancel = false;
+    //             //_isDoubleJumpReady = false;
+    //        // }
+    //     }
+
+    // }
